cfec525acbabe39f0b333c46ea6e30457c422496
RaphaelTFool <1207975808@qq.com>
2021-02-27 16:40:08 +0800

    增加easylogging使用配置文件的用例

---
+++ test/log_using_conf.cpp
@@ -0,0 +1,13 @@
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, const char** argv) {
+    // Load configuration from file
+    el::Configurations conf("default.conf");
+    // Reconfigure single logger
+    el::Loggers::reconfigureLogger("default", conf);
+    // Actually reconfigure all loggers instead
+    el::Loggers::reconfigureAllLoggers(conf);
+    // Now all the loggers will use configuration from file
+}
